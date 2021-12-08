498812d8a8c66d174f061a2a7cd2e3e61e3e6a24
wujiale-spec <1284698788@qq.com>
2021-12-08 16:33:43 +0800

    deploy

---
+++ precache-manifest.3c46a07a26e47ca2ceabd2915de72f48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c19c8fa988a58cd6d1ec",
+    "url": "/money-website/css/app.061619f9.css"
+  },
+  {
+    "revision": "d0da211c69b7053fa92e624dc34dd34d",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "c19c8fa988a58cd6d1ec",
+    "url": "/money-website/js/app.5fa83fca.js"
+  },
+  {
+    "revision": "23c3342e12deb20458c6",
+    "url": "/money-website/js/chunk-vendors.69af4100.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "043808aaf99ace4444bc91696b3239bb",
+    "url": "/money-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
